c32e8a14bd1b6b5bc779b4ea1649b1ac7b1a8413
Sam Breed <sam@quickleft.com>
2016-01-03 20:53:49 -0800

    remove dead outsideclick handler code

--- src/TagInput.jsx
+++ src/TagInput.jsx
@@ -36,57 +36,13 @@ const TagsInput = React.createClass({
     }
   },
 
-  componentWillMount() {
-    if (this.props.value && this.props.defaultValue && process.env.NODE_ENV !== 'production') {
-      console.warn('Warning: you have provided both a `value` and `defaultValue` to a form element. Check the render method of TagsInput') // eslint-disable-line no-console
-    }
-  },
-
-  componentDidMount() {
-    const menuElem = findDOMNode(this)
-    const controlElem = findDOMNode(this.refs.input)
-
-    this._closeMenuIfClickedOutside = (event) => {
-      var eventOccuredOutsideMenu = this.clickedOutsideElement(menuElem, event)
-      var eventOccuredOutsideControl = this.clickedOutsideElement(controlElem, event)
-
-      // Hide dropdown menu if click occurred outside of menu
-      if (eventOccuredOutsideMenu && eventOccuredOutsideControl) {
-        this.setState({
-          isOpen: false
-        }, this._unbindCloseMenuIfClickedOutside)
-      }
-    }
-
-    this._bindCloseMenuIfClickedOutside = _ => {
-      document.addEventListener('click', this._closeMenuIfClickedOutside)
-    }
-
-    this._unbindCloseMenuIfClickedOutside = _ => {
-      document.removeEventListener('click', this._closeMenuIfClickedOutside)
-    }
-  },
-
-  componentWillUnmount() {
-    this._unbindCloseMenuIfClickedOutside()
-  },
-
-  clickedOutsideElement(element, event) {
-    var eventTarget = (event.target) ? event.target : event.srcElement
-    while (eventTarget != null) {
-      if (eventTarget === element) return false
-      eventTarget = eventTarget.offsetParent
-    }
-    return true
-  },
-
   addTag(value) {
     if (this.props.allowOriginalValues === false
         && this.props.tags.indexOf(value) < 0) {
       return
     }
 
-    const tags = (this.props.value || this.props.defaultValue || []).concat([ value ])
+    const tags = (this.props.value || []).concat([ value ])
     this.setState({ filteredTags: [] })
 
     // Reset the input value
@@ -99,7 +55,7 @@ const TagsInput = React.createClass({
   },
 
   removeTag(value = last(this.props.value)) {
-    const tags = this.props.value || this.props.defaultValue || []
+    const tags = this.props.value || []
     if (this.props.onChange) {
       this.props.onChange(without(tags, value))
     }
@@ -107,7 +63,7 @@ const TagsInput = React.createClass({
 
   filterTags(ev) {
     const inputValue = ev.target.value
-    const currentValue = this.props.value || this.props.defaultValue || []
+    const currentValue = this.props.value || []
     if (inputValue === '') {
       this.setState({ filteredTags: [] })
       return
@@ -137,16 +93,8 @@ const TagsInput = React.createClass({
     this.setState({ filteredTags })
   },
 
-  handleInputFocus: function() {
-    this.setState({
-      isOpen: true
-    }, function() {
-      this._bindCloseMenuIfClickedOutside()
-    })
-  },
-
   render() {
-    const tags = this.props.value || this.props.defaultValue || []
+    const tags = this.props.value || []
     const childProps = {
       addTag: this.addTag,
       removeTag: this.removeTag,
